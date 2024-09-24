bf2dd00dee15b7e4f61276bbca74d8dba8d48053
DarkxSurfer <darkxurfer@gmail.com>
2024-09-24 05:23:21 +0500

    Side Menu Screen by Meez

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        themeMode: ThemeMode.dark,
+        themeMode: ThemeMode.light,
         theme: CustomTheme.lightTheme,
         darkTheme: CustomTheme.darkTheme,
         debugShowCheckedModeBanner: false,
